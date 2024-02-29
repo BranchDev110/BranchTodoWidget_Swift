cd3413f94ee0f54c2d56cb5945bcc969b81d90d1
BranchDev110 <steven.fredericks.dev110@gmail.com>
2024-02-29 02:31:27 -0500

    Add async for-in feature with ObservableObject

--- BranchTodoWidget/ContentView.swift
+++ BranchTodoWidget/ContentView.swift
@@ -8,24 +8,48 @@
 import SwiftUI
 import Accelerate
 
+class Simulation: ObservableObject {
+    @Published var step = 0
+    let nsteps = 10
+    
+    func run() async {
+        for n in 0..<nsteps {
+            await MainActor.run {
+                self.step = n
+            }
+            print("Running \(n) / \(nsteps)")
+            sleep(2)
+        }
+    }
+}
+
 struct ContentView: View {
     @State private var thefruit = ""
     @AppStorage("fruit") var fruit = ""
     
+    @StateObject private var simulation = Simulation()
+    
     var body: some View {
-        let x = [2,-3];
         VStack {
             TextField("Enter fruit", text: $thefruit).multilineTextAlignment(.center).frame(maxWidth: 200)
             Button("Save fruit") {
-                fruit = thefruit;
+                fruit = simulation.step.formatted();
             }
             Text("Saved fruit: \(fruit)")
+            
+            Text("Running \(simulation.step + 1) / \(simulation.nsteps)")
+            
+            Button("Run simulation") {
+                Task {
+                    await simulation.run()
+                }
+            }
         }
         .padding()
     }
 }
 
-struct Content∑View_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
